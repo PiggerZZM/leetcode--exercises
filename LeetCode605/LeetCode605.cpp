76e736f451143f45cc876a16cf9c47412c70ebf4
PiggerZZM <442823793@qq.com>
2020-06-23 20:46:52 +0800

    LeetCode605

---
+++ LeetCode605/LeetCode605.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=605 lang=cpp
+ *
+ * [605] 种花问题
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    bool canPlaceFlowers(vector<int> &flowerbed, int n)
+    {
+        int count = 0;
+        for (int i = 0; i < flowerbed.size(); i++)
+        {
+            if (flowerbed[i] == 0)
+            {
+                int left = i - 1;
+                int right = i + 1;
+                if (( (left >= 0 && flowerbed[left] == 0) || left == -1) && ( (right <= flowerbed.size() - 1 && flowerbed[right] == 0) || right == flowerbed.size()))
+                {
+                    count++;
+                    flowerbed[i] = 1;
+                }
+            }
+        }
+        if (count >= n)
+            return true;
+        else
+            return false;
+    }
+};
+// @lc code=end
---
+++ LeetCode605/main.cpp
@@ -0,0 +1,14 @@
+#include<vector>
+#include<cstdio>
+using namespace std;
+
+#include "LeetCode605.cpp"
+
+int main()
+{
+    vector<int> v = {1, 0, 0, 0, 0, 0, 1};
+    Solution s;
+    s.canPlaceFlowers(v, 2);
+
+    return 0;
+}
